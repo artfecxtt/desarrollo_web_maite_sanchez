7126b0cbfb4ca70ae9c1a0c341e86c08fa8e11d8
artfecxtt <maite.sanchez.g@ug.uchile.cl>
2025-08-25 22:03:34 -0400

    Feat: added validation on agregar-avisos.js, created the popout modality on 1ra-fila and modified README.md

---
+++ Tarea 1/README.md
@@ -0,0 +1,16 @@
+# CC5002-2 - Tarea 1
+Por Maite C. Sánchez Guenante
+## Detalles y decisiones excepcionales
+### 1.
+Para la parte 3, de realizar un listado de avisos de adopción, solamente la primera fila (después de la fila de títulos) 
+es funcional, esta fila se puede apretar y llevará a la página con información detallada de la misma fila, imágenes
+de la mascota en adopción y la opción de clickear la imágen para que se agrande en modo 'popout'.
+
+Las demás filas de la pagina de listado de avisos de adopción son funcionales en el sentido de que son apretables, pero
+no llevan a nada.
+
+La decisión de solamente hacer una fila completamente funcional fue ya que encontré que realizar el mismo procedimiento
+5 veces dentro de la página iba a ser tedioso, consumidor de tiempo, y poco pedagógico. La modalidad para las otras 4 filas
+debería ser la misma que se aplica en la primera fila solo que con los datos e imágenes distintas, por lo tanto, bastaría con
+realizar algún tipo de 'plantilla' que pueda modificar datos, algo que todavía no hemos aprendido en auxiliares para el 
+momento que se tiene que entregar esta tarea.
--- Tarea 1/js/agregar-avisos.js
+++ Tarea 1/js/agregar-avisos.js
@@ -108,3 +108,196 @@ regionSelect.addEventListener("change", changeComuna)
 // #########################################################################################
 
 
+
+// VALIDACIONES ############################################################################
+
+const ventEmer = document.getElementById("vent-emergente");
+
+const validarForm = () => {
+    // VALIDACIONES POR INPUT (se les entrega el variable.value menos en files)
+    const validadorRegion = (region) => region !== "";
+    const validadorComuna = (comuna) => comuna !== "";
+    const validadorSector = (sector) => sector && (sector.length <= 100);
+
+    const validadorName = (name) => name && (name.length >= 3) && (name.length <= 200);
+    const validadorEmail = (email) => {
+        const expreg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return email && email.length <= 100 && expreg.test(email);
+    }
+    const validadorCelular = (celular) => {
+        const expreg = /^\+\d{3}\.\d{8,10}$/;
+        return expreg.test(celular);
+    }
+
+    const validadorContacto = (contacto, metodo) => {
+        if(contacto !== ""){
+            return metodo.length >=4 && metodo.length <= 50
+        } else {
+            return true
+        }
+    }
+
+    const validadorTipo = (tipo) => tipo !== "";
+    const validadorCantidad = (cantidad) => {
+        const numero = Number(cantidad);
+        if (isNaN(numero) || numero < 1){
+            return false && cantidad;
+        }else{
+            return true && cantidad;
+        }
+    }
+    const validadorEdad = (edad) => {
+        const numero = Number(edad);
+        if (isNaN(numero) || numero < 1){
+            return false && edad;
+        }else{
+            return true && edad;
+        }
+    }
+    const validadorUnidad = (unidad) => unidad !== ""
+
+    // const validadorFecha = (fecha) => {}
+
+    const validadorFiles = (archivos) => archivos.files.length >= 1 && archivos.files.length <= 5;
+
+    //AHORA, el real validador
+    let isValid = false;
+    let msg = "";
+
+    let regionInput = document.getElementById("region");
+    let comunaInput = document.getElementById("comuna");
+    let sectorInput = document.getElementById("sector");
+    let nombreInput = document.getElementById("nombre");
+    let emailInput = document.getElementById("email");
+    let celularInput = document.getElementById("nro-celular");
+    let contactoInput = document.getElementById("contacto");
+    let metodoInput = document.getElementById("metodo-contacto");
+    let tipoInput = document.getElementById("tipo-mascota");
+    let cantidadInput = document.getElementById("cantidad");
+    let edadInput = document.getElementById("edad-animal");
+    let unidadInput = document.getElementById("unidad-edad");
+    let fechaInput = document.getElementById("fecha-entrega");
+    let filesInput = document.getElementById("fotos");
+
+    if(!validadorRegion(regionInput.value)){
+        msg += "Debe seleccionar una región\n"
+        regionInput.style.borderColor = "red";
+    }else{
+        regionInput.style.borderColor = "";
+    }
+
+    if(!validadorComuna(comunaInput.value)){
+        msg += "Debe seleccionar una comuna\n"
+        comunaInput.style.borderColor = "red";
+    }else{
+        comunaInput.style.borderColor = "";
+    }
+
+    if(!validadorSector(sectorInput.value)){
+        msg += "Sector inválido (debe tener como máximo 100 caracteres\n"
+        sectorInput.style.borderColor = "red";
+    }else{
+        sectorInput.style.borderColor = "";
+    }
+
+    if(!validadorName(nombreInput.value)){
+        msg += "Nombre inválido (debe tener entre 3 y 200 caracteres).\n"
+        nombreInput.style.borderColor = "red";
+    }else{
+        nombreInput.style.borderColor = "";
+    }
+
+
+    if (!validadorEmail(emailInput.value)) {
+        msg += "Email inválido o demasiado largo (máx 100 caracteres).\n";
+        emailInput.style.borderColor = "red";
+    } else {
+        emailInput.style.borderColor = "";
+    }
+
+    if (!validadorCelular(celularInput.value)) {
+        msg += "Celular inválido. Formato esperado: +XXX.XXXXXXXX.\n";
+        celularInput.style.borderColor = "red";
+    } else {
+        celularInput.style.borderColor = "";
+    }
+
+    if (!validadorContacto(contactoInput.value, metodoInput.value)){
+        msg += "ID o URL inválida (debe tener entre 4 y 50 caracteres)\n";
+        metodoInput.style.borderColor = "red";
+    } else {
+        metodoInput.style.borderColor = "";
+    }
+
+    if (!validadorTipo(tipoInput.value)) {
+        msg += "Debe seleccionar un tipo de mascota.\n";
+        tipoInput.style.borderColor = "red";
+    } else {
+        tipoInput.style.borderColor = "";
+    }
+
+    if (!validadorCantidad(cantidadInput.value)) {
+        msg += "Cantidad inválida (debe ser mayor o igual a 1).\n";
+        cantidadInput.style.borderColor = "red";
+    } else {
+        cantidadInput.style.borderColor = "";
+    }
+
+    if (!validadorEdad(edadInput.value)) {
+        msg += "Edad inválida (debe ser mayor o igual a 1).\n";
+        edadInput.style.borderColor = "red";
+    } else {
+        edadInput.style.borderColor = "";
+    }
+
+    if (!validadorUnidad(unidadInput.value)) {
+        msg += "Debe seleccionar una unidad de edad.\n";
+        unidadInput.style.borderColor = "red";
+    } else {
+        unidadInput.style.borderColor = "";
+    }
+
+    // if (!validadorFecha(fecha)){}
+
+    if (!validadorFiles(filesInput)) {
+        msg += "Debe subir entre 1 y 5 archivos.\n";
+        filesInput.style.borderColor = "red";
+    } else {
+        filesInput.style.borderColor = "";
+    }
+ 
+    if (msg !== ""){
+        alert("Errores en el formulario: \n" + msg);
+        return;
+    }
+
+    ventEmer.style.display = "flex";
+    
+
+}
+
+const vent_si = () => {
+    ventEmer.style.display = "none"
+    alert("Hemos recibido la información de adopción, muchas gracias y suerte!")
+    document.getElementById("btn-portada").hidden = false;
+}
+
+const vent_no = () => {
+    ventEmer.style.display = "none"
+}
+
+const metodo_contacto = () => {
+    document.getElementById("metodo-cont").hidden = false;
+}
+
+let sibtn = document.getElementById("btnSi");
+sibtn.addEventListener("click", vent_si)
+
+let nobtn = document.getElementById("btnNo");
+nobtn.addEventListener("click", vent_no)
+
+let enviarbtn = document.getElementById("btn-enviar");
+enviarbtn.addEventListener("click", validarForm)
+
+let contactoInput = document.getElementById("contacto");
+contactoInput.addEventListener("change", metodo_contacto)
